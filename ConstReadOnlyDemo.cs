a3a7ac7565fd7bbea3bba14f16df06d0174f59ad
chandrasenreddy.sunk <chandrasenreddy.sunk@INL-FTS4T72>
2018-01-23 22:52:32 +0530

    jan 23 demo

---
+++ ConstReadOnlyDemo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTraining
+{
+    public class ConstReadOnlyDemo
+    {
+        public ConstReadOnlyDemo(int i)
+        {
+            Login = i; // Read-only values can be changes only in constructor of the containing class.
+        }
+        public const int AdminRole = 1;
+        public const int EmployeeRole = 3;
+        public const int ManagerRole = 2;
+
+        public readonly int Access = 4;
+        public readonly int Login = 5;
+    }
+    public class ConstReadOnlyTest
+    {
+        static void Main()
+        {
+            ConstReadOnlyDemo obj = new ConstReadOnlyDemo(10);
+            //obj.Login = 4; //Errors out can not assign value to read only property(member)
+            var i = ConstReadOnlyDemo.EmployeeRole;
+            var j = obj.Access;
+            var k = obj.Login;
+            //ConstReadOnlyDemo.EmployeeRole = 2; //Can not assign value to const variables
+        }
+    }
+
+}
---
+++ refAndOutDemo.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTraining
+{
+    public class RefAndOutDemo
+    {
+        static int RefValueTest(int k)
+        {
+            k++; // k = k+1;
+            return k;
+        }
+        static int RefTest(ref int k)
+        {
+            k++; // k = k+1;
+            return k;
+        }
+        static int OutTest(out int k)
+        {
+            k = 11;
+            k++;
+            return k;
+        }
+        static void Main()
+        {
+            int i = 10;
+            int a;
+            Console.WriteLine("i value is:" + i);
+            i = RefValueTest(i); //
+            var j = RefTest(ref i);
+            //RefTest(a); //As 'a' doesn't have any memory we can not pass by reference
+
+            var l = OutTest(out i);
+            OutTest(out a);
+            Console.WriteLine("i value after method call is:" + i);
+
+            /*
+             * Access static class
+             * 
+             */
+
+            StaticDemo.EmpId = 10;
+            StaticDemo.GetAllEmployeese();
+            //StaticDemo s = new StaticDemo(); //Object can not be created on static class
+
+            NonStaticClass.GetAllEmployeese();
+
+            NonStaticClass test = new NonStaticClass();
+            
+            NonStaticClass.StaticProp = 11;
+
+            Console.ReadKey();
+        }
+    }
+
+
+    public static class StaticDemo
+    {
+        public static int EmpId;
+
+        public static void GetAllEmployeese()
+        {
+
+        }
+        //public int MyProperty { get; set; }
+        //public void Print()
+        //{
+
+        //}
+    }
+    public class NonStaticClass
+    {
+        public int MyProperty { get; set; }
+        public static int StaticProp { get; set; }
+
+        public void Print()
+        {
+
+        }
+        public static void GetAllEmployeese()
+        {
+            StaticProp = 10;
+            //MyProperty = 10;
+        }
+    }
+}
